293c5a4af8a44aa1e7c4228c6166283b02353325
lucapolzone <lucapolzone@gmail.com>
2024-07-01 16:31:33 +0200

    feat: handle empty array error

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -9,12 +9,12 @@ function ListGroup() {
     'Paris'
   ];
   
-  
+
   return (
     <Fragment>
       <h1>List</h1>
+      { items.length === 0 ? <p>No item found</p> : null }
       <ul className="list-group">
-        {/* <li className="list-group-item">An item</li> */}
         {items.map((item) => (
           <li key={item}>{item}</li>
         ))};
